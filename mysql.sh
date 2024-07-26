e03ec2a4796e4c70869074ae1a990464c31b2b2a
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-26 23:18:49 +0530

    shell msg

--- mysql.sh
+++ mysql.sh
@@ -1,9 +1,33 @@
-#!/bin/bash
+# #!/bin/bash
+# source ./commons.sh
+
+# check_root
+
+# echo "Please enter DB Password:"
+# read -s mysql_root_password
+
+# dnf install mysql-server -y &>>$LOGFILE
+
+# systemctl enable mysqld &>>$LOGFILE
+
+# systemctl start mysqld &>>$LOGFILE
+
+# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+
+# mysql -h db.lingaiah.online -uroot -p{mysql_root_password} &>>$LOGFILE
+# if [$? -ne 0]
+# then
+#    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+# else
+#    echo -e "MySQL Root password is already setup..$Y SKIPPING $N"
+# fi
+
+
 source ./commons.sh
 
 check_root
 
-echo "Please enter DB Password:"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
@@ -12,14 +36,11 @@ systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-
-mysql -h db.lingaiah.online -uroot -p{mysql_root_password} &>>$LOGFILE
-if [$? -ne 0]
+#Below code will be useful for idempotent nature
+mysql -h db.lingaiah.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
 then
-   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
-   echo -e "MySQL Root password is already setup..$Y SKIPPING $N"
+    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
-
-
