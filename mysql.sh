a2c16beef1cd7cf6c55edbb6dcc5107569c1243c
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-27 16:26:48 +0530

    expens-shell-source

--- mysql.sh
+++ mysql.sh
@@ -14,6 +14,7 @@ systemctl start mysqld &>>$LOGFILE
 
 #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
 
+#mysql -h db.lingaiah.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 mysql -h db.lingaiah.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [$? -ne 0]
 then
