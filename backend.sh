45484ff6ccf20a059cf2e14cc8fcd23562cf1e7f
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-07-27 16:11:00 +0530

    expens-shell-source

--- backend.sh
+++ backend.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+#!/bin/bash
+
 source ./common.sh
 
 check_root
 
-echo "Please enter DB Password:"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 dnf module disable nodejs -y &>>$LOGFILE
-
 dnf module enable nodejs:20 -y &>>$LOGFILE
-
 dnf install nodejs -y &>>$LOGFILE
 
 id expense &>>$LOGFILE
 if [ $? -ne 0 ]
-then 
-   useradd expense &>>$LOGFILE
+then
+    useradd expense &>>$LOGFILE
 else
-   echo -e "Expense user already created..$Y Skipping $N"
+    echo -e "Expense user already created...$Y SKIPPING $N"
 fi
 
 mkdir -p /app &>>$LOGFILE
@@ -31,18 +31,65 @@ unzip /tmp/backend.zip &>>$LOGFILE
 
 npm install &>>$LOGFILE
 
-cp /root/expense-shell-source/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+#check your repo and path
+cp /home/ec2-user/expense-shell-1/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
 
 systemctl daemon-reload &>>$LOGFILE
 
-systemctl start backend  &>>$LOGFILE
+systemctl start backend &>>$LOGFILE
 
 systemctl enable backend &>>$LOGFILE
 
 dnf install mysql -y &>>$LOGFILE
 
-mysql -h db.lingaiah.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.daws78s.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 
 systemctl restart backend &>>$LOGFILE
+
+#-------------------------------------------------------------------
+# source ./common.sh
+
+# check_root
+
+# echo "Please enter DB Password:"
+# read -s mysql_root_password
+
+# dnf module disable nodejs -y &>>$LOGFILE
+
+# dnf module enable nodejs:20 -y &>>$LOGFILE
+
+# dnf install nodejs -y &>>$LOGFILE
+
+# id expense &>>$LOGFILE
+# if [ $? -ne 0 ]
+# then 
+#    useradd expense &>>$LOGFILE
+# else
+#    echo -e "Expense user already created..$Y Skipping $N"
+# fi
+
+# mkdir -p /app &>>$LOGFILE
+
+# curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
+
+# cd /app
+# rm -rf /app/*
+# unzip /tmp/backend.zip &>>$LOGFILE
+
+# npm install &>>$LOGFILE
+
+# cp /root/expense-shell-source/backend.service /etc/systemd/system/backend.service &>>$LOGFILE
+
+# systemctl daemon-reload &>>$LOGFILE
+
+# systemctl start backend  &>>$LOGFILE
+
+# systemctl enable backend &>>$LOGFILE
+
+# dnf install mysql -y &>>$LOGFILE
+
+# mysql -h db.lingaiah.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+
+# systemctl restart backend &>>$LOGFILE
 
 
