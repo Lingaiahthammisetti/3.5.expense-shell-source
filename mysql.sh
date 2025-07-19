dff63af85293beac8df9767251c939e25c9d13fc
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-19 11:47:29 +0530

    expense shell source

--- mysql.sh
+++ mysql.sh
@@ -1,30 +1,49 @@
+# #!/bin/bash
+# source ./common.sh
+
+# check_root
+
+# echo "Please enter DB Password:"
+# #read -s mysql_root_password
+# read mysql_root_password
+
+# dnf install mysql-server -y &>>$LOGFILE
+
+# systemctl enable mysqld &>>$LOGFILE
+
+# systemctl start mysqld &>>$LOGFILE
+
+# #mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+
+# #mysql -h db.lingaiah.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+# #mysql -h mysql.lithesh.shop -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+
+# mysql -h mysql.lithesh.shop -u root -p"${mysql_root_password}" -e 'show databases;' &>>$LOGFILE
+
+# if [ $? -ne 0 ]
+# then
+#    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+# else
+#    echo -e "MySQL Root password is already setup..$Y SKIPPING $N"
+# fi
+
+
 #!/bin/bash
 source ./common.sh
 
 check_root
 
 echo "Please enter DB Password:"
-#read -s mysql_root_password
 read mysql_root_password
 
 dnf install mysql-server -y &>>$LOGFILE
-
 systemctl enable mysqld &>>$LOGFILE
-
 systemctl start mysqld &>>$LOGFILE
 
-#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-
-#mysql -h db.lingaiah.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-#mysql -h mysql.lithesh.shop -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-
 mysql -h mysql.lithesh.shop -u root -p"${mysql_root_password}" -e 'show databases;' &>>$LOGFILE
-
 if [ $? -ne 0 ]
 then
-   mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+   mysql_secure_installation --set-root-pass "${mysql_root_password}" &>>$LOGFILE
 else
    echo -e "MySQL Root password is already setup..$Y SKIPPING $N"
 fi
-
-
