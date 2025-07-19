34f7663ac9e597bdb632386394e95a9f9dfa7ef5
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-19 12:42:55 +0530

    expense shell source

--- mysql.sh
+++ mysql.sh
@@ -13,15 +13,16 @@ systemctl enable mysqld &>>$LOGFILE
 
 systemctl start mysqld &>>$LOGFILE
 
-mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+#mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 
 #Below code will be useful for idempotent nature
-mysql -h mysql.lithesh.shop -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
+mysql -h mysql.lithesh.shop -u root -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
-systemctl status mysqld
-VALIDATE $? "mysql status"
+
+systemctl status mysqld &>>$LOGFILE
+
